64d24203b6d427b015214e5f92afec2bc6e44886
LaReyna <ernest_s@bk.ru>
2026-01-08 15:17:00 +0300

    Добавил роуты для контроллеров

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :client
+  resources :master
+  resources :service
+  resources :booking
 end
