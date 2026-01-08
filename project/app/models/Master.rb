0a0781e2b8a747e7f85408d3fd5189c4c1c82251
LaReyna <ernest_s@bk.ru>
2026-01-08 14:09:44 +0300

    Создал файлы для CRUD сущностей

--- project/app/models/Master.rb
+++ project/app/models/Master.rb
@@ -1,10 +1,12 @@
 class Master < ApplicationRecord
     self.table_name = 'master'
-     has_many :bookings, foreign_key: 'master_id', dependent: :destroy
+    
+    has_many :bookings, foreign_key: 'master_id', dependent: :destroy
+
     scope :active, -> { where(is_active: true) }
     scope :inactive, -> { where(is_active: false) }
     
     validates :last_name, :first_name, :phone_number, presence: true
     validates :phone_number, uniqueness: true
-    validates :phone_number, format: { with: /\A\+?[\d\s\-\(\)]+\z/, message: "должен быть допустимым номером телефона" }
+    validates :phone_number, format: { with: /\A\+?[\d\s\-\(\)]+\z/, message: "Должен быть допустимым номером телефона" }
 end
