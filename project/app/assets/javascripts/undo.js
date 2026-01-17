1f141bc2738f940b327e8d275f27b103ba5dfdf4
LaReyna <ernest_s@bk.ru>
2026-01-17 19:52:46 +0300

    Добавил модалку для CTRL+Z

--- project/app/assets/javascripts/undo.js
+++ project/app/assets/javascripts/undo.js
@@ -1,24 +1,17 @@
-document.addEventListener('keydown', function(e) {
-  const target = e.target;
-  if (target.tagName === 'INPUT' || target.tagName === 'TEXTAREA' || target.isContentEditable) return;
+document.addEventListener('keydown', async function(e) {
+    const target = e.target;
+    if (target.tagName === 'INPUT' || target.tagName === 'TEXTAREA' || target.isContentEditable) return;
 
-  if ((e.ctrlKey || e.metaKey) && (e.key.toLowerCase() === 'z' || e.key === 'я')) {
-    e.preventDefault();
+    if ((e.ctrlKey || e.metaKey) && (e.key.toLowerCase() === 'z' || e.key === 'я')) {
+        e.preventDefault();
 
-    fetch('/undo', {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
-      }
-    })
-    .then(res => {
-      location.reload();
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      showToast('Ошибка при отмене операции');
-    });
-  }
+        await undo()
+        .then(res => {
+            location.reload();
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            showToast('Ошибка при отмене операции');
+        });
+    }
 });
