adddee539876be85ec3f7b8dce5f1068fed7e862
KuSheDuSole <maksimrusinov045@gmail.com>
2026-01-17 18:30:35 +0300

    Добавлен скрипт для создания и восстановления из бекапов, пока без возможности использования извне

--- project/backup/beauty_salon_json_backup.rb
+++ project/backup/beauty_salon_json_backup.rb
@@ -1,49 +1,94 @@
+require_relative '../db/migration_installer'
+require_relative '../config/DB_CONFIG'
 require 'json'
 
 class BeautySalonJsonBackup
     
-    def initialize(database)
-        @database = database
-        @path_to_migrations_dir = '../db/migrations/*.sql'
-        @path_to_jsons_dir = 'jsons/'
-        @tables = {}
+    def initialize
+        @path_to_backup_dir = Rails.root.join('backup', 'backup_jsons')
+        @backup_data = {}
+
     end
 
     def create_backup()
-        extract_tables_name()
-        @tables.each do |migration_number, table_name|
-            filename = create_file_name(migration_number, table_name)
-            data = get_data(table_name)
-            write_into_json(filename, data)
+        
+        backup_data = {
+            clients: Client.all.map(&:attributes),
+            masters: Master.all.map(&:attributes),
+            services: Service.all.map do |service|
+                attrs = service.attributes
+                attrs['duration'] = service.duration.to_i if service.duration
+                attrs
+            end,
+            bookings: Booking.all.map(&:attributes)
+        }
+
+        timestamp = Time.now.strftime("%Y-%m-%d_%H-%M-%S")
+        filename = "backup_#{timestamp}.json"
+        filepath = File.join(@path_to_backup_dir, filename)
+
+        File.write(filepath, JSON.pretty_generate(backup_data))
+
+        filepath
+    end
+
+    def restore_backup(filename = nil)
+
+        if filename
+            filepath = File.join(@path_to_backup_dir, filename.to_s)
+        
+            unless File.exist?(full_path)
+                raise ArgumentError, "Файл '#{filename}' не найден в директории '#{@path_to_backup_dir}'"
+            end
+        else
+            backup_files = Dir.glob(File.join(@path_to_backup_dir, "*.json"))
+
+            if backup_files.empty?
+                puts "Нет файлов бэкапа!"
+                return
+            end
+
+            filepath = backup_files.sort.last
+        end
+
+        backup_data = JSON.parse(File.read(filepath))
+
+        begin
+            Booking.delete_all
+            Client.delete_all
+            Master.delete_all
+            Service.delete_all
+
+            {
+                clients: Client,
+                masters: Master,
+                services: Service,
+                bookings: Booking
+            }.each do |key, model|
+                backup_data[key.to_s].each do |record_data|
+                    model.create!(record_data)
+                end
+            end
+
+            puts "Восстановление завершено"
+            true
+        rescue => error
+            puts "Ошибка при восстановлении: #{error.message}"
+            false
         end
     end
 
-    def extract_tables_name() 
-        sql_files = Dir.glob(@path_to_migrations_dir)
-        sql_files.each do |file_path|
-            filename = File.basename(file_path, '.sql')
-            
-            if match = filename.match(/^(\d+)_create_(.+)$/)
-                migration_number = match[1]
-                table_name = match[2]
-                next if migration_number == '000'
-                
-                @tables[migration_number] = table_name
+    def list_backups
+        backup_files = Dir.glob(File.join(@path_to_backup_dir, "*.json"))
+    
+        if backup_files.empty?
+            puts "Нет бэкапов."
+        else
+            puts "Доступные бэкапы:"
+            backup_files.sort.reverse.each do |file|
+                puts "  #{File.basename(file)} (создан: #{File.mtime(file)})"
             end
         end
     end
 
-    def get_data(table_name)
-        data = @database.execute("SELECT * FROM #{table_name}")
-    end
-
-    def write_into_json(filepath, data)
-        File.write(filepath, JSON.pretty_generate(data))
-    end
-
-     def create_file_name(migration_number, table_name)
-        filename = "#{migration_number}_backup_#{table_name}.json"
-        filepath = File.join(@path_to_jsons_dir, filename)
-     end
-        
 end
