8c74a13c5b5641822c428864d65f433388ba4d05
VishnyakovDaniil <danya.vishnyakov.2006@mail.ru>
2026-01-18 20:02:11 +0300

    добавлена фича с кешированием отчетов, протестировано. Паттерны: Proxy, Singleton, Template method

---
+++ project/app/reports_service/report_service_interface.rb
@@ -0,0 +1,19 @@
+module ReportServiceInterface
+
+    def master_efficiency_report(start_date, end_date, status = 'Выполнена', is_active=true)
+        raise NotImplementedError, "This method should be implemented in a subclass"
+    end
+
+    def client_analytics_report(start_date, end_date)
+        raise NotImplementedError, "This method should be implemented in a subclass"
+    end
+
+    def operational_report(start_date, end_date)
+        raise NotImplementedError, "Subclass must implement this method"
+    end
+    
+    def invalidate_cache
+        raise NotImplementedError, "Subclass must implement this method"
+    end
+    
+end
